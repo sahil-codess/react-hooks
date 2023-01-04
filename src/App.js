8090f76f8cb502dca492b45c5bd947b899270914
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-04 13:06:57 +0430

    used function hooks instead of class components to set the counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import ClassCounter from './components/ClassCounter';
-import './App.css';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      {/* <ClassCounter /> */}
+      <HookCounter />
     </div>
   );
 }
