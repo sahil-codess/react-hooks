3de0c850503fccd3c34ae84d44170e1362a6b495
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-04 20:18:02 +0430

    used useState in function hooks, one key point that setState does not automatically merge and update the object properties we have to do it manually

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import './App.css'
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
     </div>
   );
 }
---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function HookCounterThree() {
+    const [name, setName] = useState({firstName: '', lastName: ''})
+
+  return (
+    <div>
+        <input type="text" value={name.firstName} onChange={e => setName({...name, firstName: e.target.value})} />
+        <input type="text" value={name.lastName} onChange={e => setName({...name ,lastName: e.target.value})} />
+        <h2>Your FirstName is : {name.firstName}</h2>
+        <h2>Your LastName is : {name.lastName}</h2>
+        <h2>{JSON.stringify(name)}</h2>
+    </div>
+  )
+}
+
+export default HookCounterThree
