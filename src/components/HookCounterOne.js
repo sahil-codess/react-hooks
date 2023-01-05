74fc45c004af9149a233426324e5101e0feac9e6
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-05 22:06:12 +0430

    conditionaly using useEffect in both class and functional components

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from 'react'
 function HookCounterOne() {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('UseEffect - updating the document title')
         document.title = `Clicked ${count} times`
-    })
+    },[count])
 
   return (
     <div>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
         <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   )
