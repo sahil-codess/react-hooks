bd2738d8457c9caccddd8a747b5debab72e5100e
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-06 20:44:09 +0430

    fetching data with usseffect using json placeholder posts data with axios package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
-import IntervalClassCounter from './components/IntervalClassCounter'
-import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
+
 
 function App() {
   return (
     <div className="App">
-       <IntervalClassCounter />
-       <IntervalHookCounter />
+       <DataFetching />
     </div>
   );
 }
