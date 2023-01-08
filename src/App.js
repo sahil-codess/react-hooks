d9a7bc6deb0d30d435ceb63f8f72ed2f418ffe90
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-08 20:07:05 +0430

    performance optimization using React.memo and using callback hook to reder the components dependent on its props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css'
-import DataFetchingOne from './components/DataFetchingOne';
-import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
+
 
 function App() {
 
   return (
     <div> 
-      {/* <DataFetchingOne /> */}
-      <DataFetchingTwo />
+      <ParentComponent />
     </div>
   );
 }
---
+++ src/components/Count.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Count({text, Count}) {
+    console.log(`Rendering ${text}`)
+  return <div>{text} - {Count}</div>
+}
+
+export default React.memo(Count)
