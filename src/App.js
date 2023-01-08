019aa23e92ab92ad66c83a3cff286e9c756c38f5
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-08 10:25:19 +0430

    data fetching with usereducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'
 import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 function App() {
 
   return (
     <div> 
-      <DataFetchingOne />
+      {/* <DataFetchingOne /> */}
+      <DataFetchingTwo />
     </div>
   );
 }
